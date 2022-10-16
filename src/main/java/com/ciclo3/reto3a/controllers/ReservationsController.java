8bb203f3cf341ccc4986021cfaf8947b975dc25e
JhonnersP <JhonnersEcheverry0708@gmail.com>
2022-10-16 10:45:10 -0500

    Reto3 A

--- src/main/java/com/ciclo3/reto3a/controllers/ReservationsController.java
+++ src/main/java/com/ciclo3/reto3a/controllers/ReservationsController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/Reservation")
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+//@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 public class ReservationsController {
 
     @Autowired
